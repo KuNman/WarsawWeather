f833b08b6c402b68d13a14d7f99b78e5cdc4e69b
KuNman <knejman56@gmail.com>
2017-10-12 12:39:38 +0200

    Branches test

--- Symfony/WarsawWeather/src/AppBundle/Controller/DefaultController.php
+++ Symfony/WarsawWeather/src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,20 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/test")
      */
-    public function indexAction(Request $request)
+    public function numberAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
     }
 }
