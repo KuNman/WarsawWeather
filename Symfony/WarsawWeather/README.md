4d9324c4410a3aeceeaecbec12aa4795ad1a4b55
KuNman <knejman56@gmail.com>
2017-10-12 12:22:15 +0200

    Initial commit

---
+++ Symfony/WarsawWeather/README.md
@@ -0,0 +1,4 @@
+WarsawWeather
+=============
+
+A Symfony project created on October 12, 2017, 12:20 pm.
---
+++ Symfony/WarsawWeather/src/AppBundle/Entity/Weather.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Weather
+ *
+ * @ORM\Table(name="weather")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\WeatherRepository")
+ */
+class Weather
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="City", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="Date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Weather
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Weather
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
